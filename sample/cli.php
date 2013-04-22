9f9c8d1ab7d25d1ebc935657a1c5ae837b912d01
peec <kjelkenes@gmail.com>
2013-04-22 03:19:18 +0200

    Removed sample. Moved to own repo (minibase-sample)

--- sample/cli.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-$mb = Minibase\MB::cli()->loadConfigFile(__DIR__ . '/app/app.json', __DIR__ . '/app');
-$mb->start();
--- sample/www/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ .'/../vendor/autoload.php';
-
-$mb = Minibase\MB::create()->loadConfigFile(__DIR__ . '/../app/app.json', __DIR__ . '/../app');
-
-$mb->start();
