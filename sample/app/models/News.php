bea30611f3d66f92c60cd700b9f0d44d60e2ec5d
peec <kjelkenes@gmail.com>
2013-04-22 02:49:51 +0200

    Add working sample (included Doctrine and twig plugin in the sample).

---
+++ sample/app/models/News.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\models;
+
+/**
+ * @Entity @Table(name="news")
+ */
+class News {
+	
+	/** @Id @Column(type="integer") @GeneratedValue **/
+	protected $id;
+	/** @Column(type="string") **/
+	protected $title;
+	
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+	public function getTitle () {
+		return $this->title;
+	}
+	
+	public function getId () {
+		return $this->id;
+	}
+	
+}
